737f264b86628a83f9257d2e9feadfd6fe1bf510
Martastain <martas@imm.cz>
2018-03-21 12:38:05 +0100

    screenshot scp upload (settings must go to ~/.xsessionrc)

--- home/.config/scripts/screenshot.sh
+++ home/.config/scripts/screenshot.sh
@@ -23,4 +23,15 @@ fi
 
 name="shot-$(hostname)-%Y%m%d-%H%M%S.png"
 scrot $name $flags -e "mv \$f $screenshots_dir && echo -n \$f | xclip"
+
+
+if [[ "${SCREENSHOT_TARGET}" ]]; then
+    scp $SCREENSHOT_TARGET_OPTS "$screenshots_dir/$(xclip -o)" "$SCREENSHOT_TARGET"
+fi
+
+if [[ "${SCREENSHOT_PREFIX}" ]]; then
+    echo ${SCREENSHOT_PREFIX}$(xclip -o) | xclip
+fi
+
+xclip -o | xclip -selection clipboard
 notify-send -u low -t 2 "Screenshot created" "$(xclip -o)"
